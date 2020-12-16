cbdb5dc54a9afb7118047ba51828cb78d1577c50
GayatriPachkawde <gayatri5kawde@gmail.com>
2020-12-16 12:32:27 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
-const express = require('express')
-const app = express()
-const port = 8080
-
-const onePageArticleCount = 10
-
+const express = require("express");
+const app = express();
+const port = 8080;
+const { newsArticleModel } = require("./connector");
+const onePageArticleCount = 10;
 
 // Parse JSON bodies (as sent by API clients)
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+app.get("/newFeeds", async (req, res) => {
+  const limit = Number(req.params.limit || 10);
+  const offset = Number(req.params.offset || 0);
+  const data = await newsArticleModel.find({}).skip(offset).limit(limit);
 
+  res.send(data);
+});
 
-app.listen(port, () => console.log(`App listening on port ${port}!`))
+app.listen(port, () => console.log(`App listening on port ${port}!`));
 
 module.exports = app;
